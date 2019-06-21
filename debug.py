87a7653fea00f2c242afaec6cc2cd3d68a8af5d6
Eric <ericbacustica@gmail.com>
2019-06-21 14:29:43 -0300

    New things: geometry (.dae, .mat), new objects, statistical acoustics

--- debug.py
+++ debug.py
@@ -3,12 +3,12 @@ import sys
 from pprint import pprint
 
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
-print('Working dir: {}'.format(os.getcwd()))
-print('sys.path:')
-pprint(sys.path)
+#print('Working dir: {}'.format(os.getcwd()))
+#print('sys.path:')
+#pprint(sys.path)
 
 
-from ra.ra import main
+from ra.ra2 import main
 
 
 main()
